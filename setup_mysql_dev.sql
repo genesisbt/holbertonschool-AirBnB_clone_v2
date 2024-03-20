30d00a348d5d684fbbaf96b9d6ba868fe6ec4f58
German <gerchureloco@gmail.com>
2024-03-20 08:59:35 -0700

    comments added

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,8 @@
+-- Create database if not exists
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Create user if not exists
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant all privileges to created user on created database
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grant select privileges to created user on performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
