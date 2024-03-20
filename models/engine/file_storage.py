5c696f3217855d80b240c80d159bdff88c54a4c2
German <gerchureloco@gmail.com>
2024-03-20 09:18:04 -0700

    storage changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,20 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def all(self, cls=None):
+        """Returns a list of objects of cls type"""
+        if cls is None:
+            return list(FileStorage.__objects.values())
+        objectsofcls = []
+        for obj in FileStorage.__objects.values():
+            if isinstance(obj, cls):
+                objectsofcls.append(obj)
+        return objectsofcls
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it exists"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
