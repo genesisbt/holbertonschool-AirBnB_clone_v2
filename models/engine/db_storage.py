fc14621f48f43fe9ab0b2b3a979a6ab19d7cc55b
German <gerchureloco@gmail.com>
2024-03-24 11:24:58 -0700

    fix create engine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-from models.city import City
-from models.state import State
+from sqlalchemy import *
+from sqlalchemy.orm import *
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
 from models.user import User
-from models.place import Place
+from models.state import State
+from models.city import City
 from models.amenity import Amenity
+from models.place import Place
 from models.review import Review
-from models.base_model import BaseModel, Base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
 import os
 
 
@@ -16,40 +18,45 @@ class DBStorage:
     __session = None
 
     def __init__(self):
-        user = os.environ.get("HBNB_MYSQL_USER")
-        password = os.environ.get("HBNB_MYSQL_PWD")
-        host = os.environ.get("HBNB_MYSQL_HOST")
-        database = os.environ.get("HBNB_MYSQL_DB")
-        env = os.environ.get("HBNB_ENV")
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}:3306/{}".
-                                      format(user, password, host, database),
-                                      pool_pre_ping=True)
-
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(getenv('HBNB_MYSQL_USER'), getenv('HBNB_MYSQL_PWD'), getenv('HBNB_MYSQL_HOST')), pool_pre_ping=True)
+        
         Session = sessionmaker(bind=self.__engine)
-        Session.configure(bind=self.__engine)
         self.__session = Session()
 
-        if env == "test":
+        
+        if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
+
         Base.metadata.create_all(self.__engine)
 
-
+    def all(self, cls=None):
+        """Query objects from the database session by the class name."""
+        objects = {}
+        classes = [cls] if cls else [BaseModel, User, State, City, Amenity, Place, Review]
+        for cls in classes:
+            query_result = self.__session.query(cls).all()
+            for obj in query_result:
+                key = f"{obj.__class__.__name__}.{obj.id}"
+                objects[key] = obj
+        return objects
+    
     def new(self, obj):
-        self.__session.add(obj)
+        """Add object to current db session."""
+        if obj:
+            self.__session.add(obj)
 
     def save(self):
+        """Commit changes of current db session."""
         self.__session.commit()
 
     def delete(self, obj=None):
-        if obj is None:
-            return
-        else:
+        """Delete obj from current db session"""
+        if obj:
             self.__session.delete(obj)
+            self.__session.commit()
 
     def reload(self):
-        from sqlalchemy.ext.declarative import declarative_base
-
-        Base = declarative_base()
-        s = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(s)
+        """Reloads the database state."""
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(session_factory)
         Base.metadata.create_all(self.__engine)
