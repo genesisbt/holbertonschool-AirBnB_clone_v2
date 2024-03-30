fb61318cd0f66c182193b89863a87161da0d3438
German <gerchureloco@gmail.com>
2024-03-29 21:15:15 -0700

    fix console

--- console.py
+++ console.py
@@ -2,19 +2,18 @@
 """ Console Module """
 import cmd
 import sys
-from models.base_model import BaseModel
 
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
-
+    from models.base_model import BaseModel
+    from models.user import User
+    from models.place import Place
+    from models.state import State
+    from models.city import City
+    from models.amenity import Amenity
+    from models.review import Review
     # determines prompt for interactive/non-interactive modes
     prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
 
@@ -141,6 +140,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_show(self, args):
         """ Method to show an individual object """
+        from models.__init__ import storage
         new = args.partition(" ")
         c_name = new[0]
         c_id = new[2]
