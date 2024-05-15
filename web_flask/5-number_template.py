1ae62330a3e7b46c4f749f00c9393c776de6f978
German <gerchureloco@gmail.com>
2024-05-15 09:17:51 -0700

    5 fix try

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -19,24 +19,24 @@ def greet_hbnb():
 @wapp.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     text = text.replace('_', ' ')
-    return ("C {}".format(text))
+    return "C {}".format(text)
 
 
-@wapp.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@wapp.route('/python', strict_slashes=False)
 @wapp.route('/python/<text>', strict_slashes=False)
-def py_route(text):
+def py_route(text='is cool'):
     text = text.replace('_', ' ')
-    return ("Python {}".format(text))
+    return "Python {}".format(text)
 
 
 @wapp.route('/number/<int:n>', strict_slashes=False)
 def n_route(n):
-    return ("{} is a number".format(n))
+    return "{} is a number".format(n)
 
 
 @wapp.route('/number_template/<int:n>', strict_slashes=False)
 def n_template(n):
-    return (render_template('5-number.html', n=n))
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
