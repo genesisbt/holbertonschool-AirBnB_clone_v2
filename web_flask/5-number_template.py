8e1fcf9000c3d930c602b01ba01e6c9469e6a9ad
German <gerchureloco@gmail.com>
2024-05-15 09:09:39 -0700

    4 and 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Script that starts a Flask web application"""
+from flask import Flask, render_template
+
+
+wapp = Flask(__name__)
+
+
+@wapp.route('/', strict_slashes=False)
+def greetings():
+    return "Hello HBNB!"
+
+
+@wapp.route('/hbnb', strict_slashes=False)
+def greet_hbnb():
+    return "HBNB"
+
+
+@wapp.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    text = text.replace('_', ' ')
+    return ("C {}".format(text))
+
+
+@wapp.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@wapp.route('/python/<text>', strict_slashes=False)
+def py_route(text):
+    text = text.replace('_', ' ')
+    return ("Python {}".format(text))
+
+
+@wapp.route('/number/<int:n>', strict_slashes=False)
+def n_route(n):
+    return ("{} is a number".format(n))
+
+
+@wapp.route('/number_template/<int:n>', strict_slashes=False)
+def n_template(n):
+    return (render_template('5-number.html', n=n))
+
+
+if __name__ == "__main__":
+    wapp.run(host='0.0.0.0', port=5000)
