195494a21129387d0ad95a49ee7a3578d36eeb33
German <gerchureloco@gmail.com>
2024-05-15 08:58:04 -0700

    pycodestyle 1 and 2

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,11 +2,14 @@
 """Starts a Flask Web app"""
 from flask import Flask
 
+
 wapp = Flask(__name__)
+
 
 @wapp.route('/', strict_slashes=False)
 def greetings():
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     wapp.run(host='0.0.0.0', port=5000)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,17 +2,21 @@
 """Starts Flask Web App"""
 from flask import Flask
 
+
 wapp = Flask(__name__)
+
 
 @wapp.route('/', strict_slashes=False)
 def greetings():
     """Method that displays "Hello HBNB!"."""
     return "Hello HBNB!"
 
+
 @wapp.route('/hbnb', strict_slashes=False)
 def greet_hbnb():
     """Method that displays "HBNB"."""
     return "HBNB"
+
 
 if __name__ == "__main__":
     wapp.run(host='0.0.0.0', port=5000)
