68fa25a9d26466b9c41dd871b2b4417bc8896035
German <gerchureloco@gmail.com>
2024-07-09 22:07:08 +0000

    5 number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,34 +1,43 @@
 #!/usr/bin/python3
-""" 5 number template """
+"""Script"""
 from flask import Flask, render_template
 
-app = Flask(__name__)
 
-@app.route('/')
-def hello_hbnb():
-    return 'Hello HBNB!'
+wapp = Flask(__name__)
 
-@app.route('/hbnb')
-def hbnb():
-    return 'HBNB'
 
-@app.route('/c/<text>')
-def c(text):
-    text_with_spaces = ' '.join(text.split('_'))
-    return f'C {text_with_spaces}'
+@wapp.route('/', strict_slashes=False)
+def greetings():
+    return "Hello HBNB!"
 
-@app.route('/python/<text>')
-def python(text):
-    text_with_spaces = ' '.join(text.split('_'))
-    return f'Python {text_with_spaces}'
 
-@app.route('/number/<int:n>')
-def number(n):
-    return f'{n} is a number'
+@wapp.route('/hbnb', strict_slashes=False)
+def greet_hbnb():
+    return "HBNB"
 
-@app.route('/number_template/<int:n>')
-def number_template(n):
-    return render_template('number.html', n=n)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, strict_slashes=False)
+@wapp.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    text = text.replace('_', ' ')
+    return ("C {}".format(text))
+
+
+@wapp.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@wapp.route('/python/<text>', strict_slashes=False)
+def py_route(text):
+    text = text.replace('_', ' ')
+    return ("Python {}".format(text))
+
+
+@wapp.route('/number/<int:n>', strict_slashes=False)
+def n_route(n):
+    return ("{} is a number".format(n))
+
+
+@wapp.route('/number_template/<int:n>', strict_slashes=False)
+def n_template(n):
+    return (render_template('5-number.html', n=n))
+
+
+if __name__ == "__main__":
+    wapp.run(host='0.0.0.0', port=5000)
