a6c8d0761b7d254814161128418d273f8fffb003
German <gerchureloco@gmail.com>
2024-07-09 00:28:58 +0000

    try

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -3,31 +3,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello HBNB!"
+def hello_hbnb():
+    return 'Hello HBNB!'
 
 @app.route('/hbnb')
 def hbnb():
-    return "HBNB"
+    return 'HBNB'
 
 @app.route('/c/<text>')
-def c_text(text):
-    text = text.replace('_', ' ')
-    return f"C {text}"
+def c(text):
+    text_with_spaces = ' '.join(text.split('_'))
+    return f'C {text_with_spaces}'
 
-@app.route('/python/')
 @app.route('/python/<text>')
-def python_text(text="is cool"):
-    text = text.replace('_', ' ')
-    return f"Python {text}"
+def python(text):
+    text_with_spaces = ' '.join(text.split('_'))
+    return f'Python {text_with_spaces}'
 
 @app.route('/number/<int:n>')
 def number(n):
-    return f"{n} is a number"
+    return f'{n} is a number'
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
-    return render_template("5-number.html", number=n)
+    return render_template('number.html', n=n)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True, strict_slashes=False)
