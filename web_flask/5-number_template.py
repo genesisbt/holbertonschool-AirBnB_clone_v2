2d58fd4f330e67427b98f6e9c3b667d875dcca6d
German <gerchureloco@gmail.com>
2024-07-09 22:12:47 +0000

    fix 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -2,25 +2,20 @@
 """Script"""
 from flask import Flask, render_template
 
-
 wapp = Flask(__name__)
-
 
 @wapp.route('/', strict_slashes=False)
 def greetings():
     return "Hello HBNB!"
 
-
 @wapp.route('/hbnb', strict_slashes=False)
 def greet_hbnb():
     return "HBNB"
-
 
 @wapp.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     text = text.replace('_', ' ')
     return ("C {}".format(text))
-
 
 @wapp.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @wapp.route('/python/<text>', strict_slashes=False)
@@ -28,16 +23,14 @@ def py_route(text):
     text = text.replace('_', ' ')
     return ("Python {}".format(text))
 
-
 @wapp.route('/number/<int:n>', strict_slashes=False)
 def n_route(n):
     return ("{} is a number".format(n))
 
-
 @wapp.route('/number_template/<int:n>', strict_slashes=False)
 def n_template(n):
-    return (render_template('5-number.html', n=n))
-
+    print(f"Rendering template for number: {n}")
+    return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
     wapp.run(host='0.0.0.0', port=5000)
